ad4c78138923b8d7ecf6602879386aa810b79f27
anupamsdesk <anupamsdesk@gmail.com>
2014-02-13 10:43:06 +0530

    adding subContainer

--- js/app.js
+++ js/app.js
@@ -4,13 +4,15 @@ var app = (function(Backbone,$,_){
 	console.log('Hello world!');
 	var id = '_partial_backbone'
 	
+	var containerId = '#subContent';
 	$.ajax({
 			url: 'partials/backbone.html',
 			method: 'GET',
             dataType: 'text',
             async: false,
 			success: function(data){
-				console.log('succeeded!');
+				console.log('succeeded!');		
+				$(containerId).html(data);
 			},
 			failure: function(){
 				console.log('failed!');
