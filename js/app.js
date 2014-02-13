f6ba4296a00397e02658c3fd984a62395836bacf
anupamsdesk <anupamsdesk@gmail.com>
2014-02-13 10:22:22 +0530

    basic setup

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@
 
 var app = (function(Backbone,$,_){
 	console.log('Hello world!');
+	var id = '_partial_backbone'
+	
+	$.ajax({
+			url: 'partials/backbone.html',
+			method: 'GET',
+            dataType: 'text',
+            async: false,
+			success: function(data){
+				console.log('succeeded!');
+			},
+			failure: function(){
+				console.log('failed!');
+			}	
+		});
 })(Backbone,$,_);
+
